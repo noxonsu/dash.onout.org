fd51ebb35ac2c14a9600ff1c5d847c41ce2f4885
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-01-28 15:02:42 +0600

    add build folder

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.64:5000'
+  baseURL: 'https://dashapi.onout.org'
 });
 
 export default axiosInstance;
