47e4f4ef807b08d95dd6b6b8fe481b6cdb106510
NotEternal <cent1pede@protonmail.ch>
2022-03-07 13:33:46 +0100

    Plugin script: add static version zip

--- src/scripts/updatePlugins.sh
+++ src/scripts/updatePlugins.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+# TODO: make it work on Windows
 # TODO: how to make key-value structure?
 SOURCES=("noxonsu" "noxonsu" "noxonsu" "noxonsu" "swaponline" "noxonsu")
 REPO_NAMES=("DAOwidget" "definance" "farmfactory" "LotteryFactory" "multi-currency-wallet-pro" "NFTsy")
 PLUGIN_IDS=("daofactory" "definance" "farmfactory" "lotteryfactory" "multicurrencywallet" "nftmarketplace")
+# paths relative to the root of the repository 
+# empty string means there is no build or we don't have the static version (or i didn't find it o_o)
+# TODO: check the VUE app (vendor/dist) for NFT repo and add the path
+BUILD_PATH=("build" "vendor_source" "" "" "vendors/swap" "")
+
+if [[ ! -d ../assets/plugins ]]; then
+  mkdir -p ../assets/plugins
+fi
 
 for ((i = 0; i < ${#REPO_NAMES[@]}; i++)); do
   SOURCE=${SOURCES[$i]}
   NAME=${REPO_NAMES[$i]}
   ID=${PLUGIN_IDS[$i]}
+  BUILD=${BUILD_PATH[$i]}
 
   echo ""
   echo "Update: $NAME.git"
@@ -16,10 +26,18 @@ for ((i = 0; i < ${#REPO_NAMES[@]}; i++)); do
 
   git clone "https://github.com/$SOURCE/$NAME.git"
 
-  if [[ ! -d ../assets/plugins ]]; then
-    mkdir -p ../assets/plugins
+  # if build path not empty
+  if [[ ! -z "$BUILD" ]]; then
+    mkdir -p "static_$ID"
+    cp -r $NAME/$BUILD/* "static_$ID"
+    zip -9 -q -r static_$ID.zip "static_$ID"
+    mv static_$ID.zip ../assets/plugins/
+
+    rm -rf "static_$ID"
   fi
 
-  zip -9 -q -r $ID.zip ./$NAME -x '*.git*' && mv $ID.zip ../assets/plugins/
+  zip -9 -q -r $ID.zip ./$NAME -x '*.git*'
+  mv $ID.zip ../assets/plugins/
+
   rm -rf ./$NAME
 done
