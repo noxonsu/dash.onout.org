e1b445318428f7271516f1e88383478713486170
Nasirdin <nasirdin.zhaparkulov@gmail.com>
2022-04-07 16:04:59 +0600

    error correction

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -1,14 +1,15 @@
 import { useContext, useState } from "react";
 import { Web3ConnecStateContext } from "../WithWeb3Connect";
+import { Route, Routes, useLocation } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 import ProductList from "../ProductList";
 import UserProducts from "../UserProducts";
 import Product from "../Product";
-
-import "./index.css";
-import { Route, Routes, useLocation } from "react-router-dom";
 import Tabs from "../Tabs";
 import Affiliate from "../Affiliate";
+
+import "./index.css";
+
 const Sections = () => {
   const location = useLocation();
 
@@ -18,27 +19,27 @@ const Sections = () => {
 
   // For now, while we save it in localStorage, retrive all saved user products from here
 
-
   if (!signed || !subscribed || account.wrongNetwork) return null;
 
-  const locationArr = location.pathname.split('/')
+  const locationArr = location.pathname.split("/");
   let newView;
-  if(locationArr !== undefined || null) {
-    newView = locationArr[locationArr.length -1];
+  if (locationArr !== undefined || null) {
+    newView = locationArr[locationArr.length - 1];
   } else {
-    newView = view
+    newView = view;
   }
-  
 
-  
   return (
     <div>
-      <Tabs newView={newView}/>
+      <Tabs newView={newView} />
       <Routes>
-        <Route path='/' element={<ProductList/>} />
-        <Route path='/presale' element={<ProductList/>} />
-        <Route path='/user-products' element={<UserProducts />} />
-        <Route path={`/products/${newView}`} element={<Product id={newView} />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/presale" element={<ProductList />} />
+        <Route path="/user-products" element={<UserProducts />} />
+        <Route
+          path={`/products/${newView}`}
+          element={<Product id={newView} />}
+        />
         <Route path={`/affiliate`} element={<Affiliate />} />
       </Routes>
     </div>
--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,13 +1,14 @@
 import { useCallback, useEffect, useContext, useState } from "react";
-import GA from 'react-ga';
+import GA from "react-ga";
 import { Web3ConnecStateContext } from "../WithWeb3Connect";
-import { PRODUCTS} from "../../constants";
+import { PRODUCTS } from "../../constants";
 import useUser from "../../hooks/useUser";
 import { UserActions } from "../UserProvider";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./index.css";
-const Tabs = ({newView} : any) => {
+
+const Tabs = ({ newView }: any) => {
   const { account } = useContext(Web3ConnecStateContext);
   const { state, dispatch } = useUser();
   const { signed, subscribed, view, products } = state;
@@ -39,63 +40,62 @@ const Tabs = ({newView} : any) => {
 
   if (!signed || !subscribed || account.wrongNetwork) return null;
 
-
   return (
     <div className="tabs">
-    <Link
-      to='/'
-      className={`tabBtn ${newView  === "" ? "active" : ""}`}
-      onClick={() => {
-        dispatch({
-          type: UserActions.changeView,
-          payload: "products",
-        });
-
-        GA.event({
-          category: 'Pages Section',
-          action: 'Open Product list'
-        });
-      }}
-    >
-      Products
-    </Link>
-    <Link
-      to='/presale'
-      className={`tabBtn ${newView  === "presale" ? "active" : ""}`}
-      onClick={() => {
-        dispatch({
-          type: UserActions.changeView,
-          payload: "presale",
-        });
-
-        GA.event({
-          category: 'Pages Section',
-          action: 'Open Presale list'
-        });
-      }}
-    >
-      Presale
-    </Link>
-    {signed && (
       <Link
-      to='/user-products'
-      className={`tabBtn ${newView  === "user-products" ? "active" : ""}`}
+        to="/"
+        className={`tabBtn ${newView === "" ? "active" : ""}`}
         onClick={() => {
           dispatch({
             type: UserActions.changeView,
-            payload: "userProducts",
+            payload: "products",
           });
 
           GA.event({
-            category: 'Page Sections',
-            action: `Open User's Products`
+            category: "Pages Section",
+            action: "Open Product list",
           });
         }}
       >
-        My products
+        Products
       </Link>
-    )}
-  </div>
+      <Link
+        to="/presale"
+        className={`tabBtn ${newView === "presale" ? "active" : ""}`}
+        onClick={() => {
+          dispatch({
+            type: UserActions.changeView,
+            payload: "presale",
+          });
+
+          GA.event({
+            category: "Pages Section",
+            action: "Open Presale list",
+          });
+        }}
+      >
+        Presale
+      </Link>
+      {signed && (
+        <Link
+          to="/user-products"
+          className={`tabBtn ${newView === "user-products" ? "active" : ""}`}
+          onClick={() => {
+            dispatch({
+              type: UserActions.changeView,
+              payload: "userProducts",
+            });
+
+            GA.event({
+              category: "Page Sections",
+              action: `Open User's Products`,
+            });
+          }}
+        >
+          My products
+        </Link>
+      )}
+    </div>
   );
 };
 
