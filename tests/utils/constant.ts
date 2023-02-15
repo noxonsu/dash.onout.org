80879b717d63d055b57ad5c5f447b90af0e499d5
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-02-15 16:55:19 +0300

    Update constant.ts

--- tests/utils/constant.ts
+++ tests/utils/constant.ts
@@ -1,7 +1,7 @@
 export const IPFS_DOMAINS = [
   ['https://dash.onout.org/', 'OnOut_Dashboard', 'connect-button'],
   ['https://farm.wpmix.net/daofactory/', 'Dao_Widget', 'connect-button'],
-  ['https://lottery.wpmix.net/', 'Lottery', 'connect-wallet'],
+  ['https://lottery.onout.org/', 'Lottery', 'connect-wallet'],
   ['https://crosschain.onout.org/', 'Crosschain', 'connect-wallet'],
   ['https://wallet.wpmix.net/', 'MCWallet', 'connect-wallet'],
   ['https://definance.wpmix.net/', 'DEX', 'connect-wallet'],
